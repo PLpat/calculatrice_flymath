dfbb7f2f6fe5c03ae27cc24336fdbaa3ef2c05b4
plpat <plpat@159.31.75.86>
2023-10-06 15:49:25 +0200

    hello

--- test/src/test/Calcul.java
+++ test/src/test/Calcul.java
@@ -3,11 +3,9 @@ package test;
 public class Calcul {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		return args;
 		
-		int b;
-		int d;
+	System.out.println("hello");
+	
 	}
 
 }
